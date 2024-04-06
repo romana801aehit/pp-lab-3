4440df07392c1113ca1645135ffe535026de1d18
romana801aehit <romana801_aehit@students.vizja.pl>
2024-04-07 00:03:49 +0200

    Go

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Point dot = new Point(15.0, 30.0);
+        System.out.println("X: " + dot.getX());
+        System.out.println("Y: " + dot.getY());
+    }
+}
